d285e71228f99d5ce9d19087d80679c95cddff84
Jhony Lucas <jhonyjl37@gmail.com>
2014-10-13 10:27:49 -0300

    Issue #18  Botao do twitter adicionado.
     Mudanças a serem submetidas:
    	modificado: app/helpers/jobs_helper.rb
    	modificado: app/views/jobs/show.html.erb
    	modificado: app/views/layouts/application.html.erb

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,14 @@
 module JobsHelper
+  def link_to_twitter(title)
+    tag_a = []
+    url = CGI.escape(request.url)
+    tag_a << %(<a href="https://twitter.com/intent/tweet?)
+    tag_a << %(hashtags=RubyJobs&amp;)
+    tag_a << %(original_referer=#{url}&amp;)
+    tag_a << %(text=Vaga%20para%20#{CGI.escape(title)}&amp;)
+    tag_a << %(tw_p=tweetbutton&amp;url=#{url}")
+    tag_a << %(class="btn btn-default" target="_blank" >)
+    tag_a << %(<i class="fa fa-twitter fa-lg"> </i>  Tweet</a>)
+    tag_a.join.html_safe
+  end
 end
