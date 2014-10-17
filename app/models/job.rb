ce6b0ed680afe6c9ec8af8601ffce95d581956b3
Luiz Cezer <lccezinha@gmail.com>
2014-10-16 22:12:56 -0300

    Validação de email + novos specs

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,6 +3,7 @@ class Job < ActiveRecord::Base
 
   validates :title, :description, :email, :company, presence: true
   validates :remote, inclusion: { in: [true, false] }
+  validates :email, format: { with: /\A[a-z]([\.\-]?\w+)+@[a-z]([\.\-]?\w+){2,}\Z/ }
 
   default_scope { order(id: :desc) }
   scope :updated_at_desc, -> { order(updated_at: :desc) }
