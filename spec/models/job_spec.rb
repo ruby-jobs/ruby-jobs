aa0e2cc396dec691ab1fa6ce1a7ff252842399b5
Eduardo Maia <eduvimaia@gmail.com>
2014-10-16 22:12:57 -0300

    included slug tests and refactoring in job_spec

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
 describe Job do
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:description) }
-  it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:company) }
-  it { should_not allow_value(nil).for(:remote) }
+  subject { create(:job, title: 'desenvolvedor back-end ruby on rails pleno') }
 
-  context 'when job is valid' do
-    let(:job) { FactoryGirl.build(:job) }
-    it 'return true' do
-      expect(job).to be_valid
-    end
+  describe 'validations' do
+    it { should validate_presence_of(:slug) }
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:company) }
+    it { should_not allow_value(nil).for(:remote) }
+  end
+
+  describe 'when job is valid' do
+    it { expect(subject).to be_valid }
+    it { expect(subject.slug).to eql "#{subject.id}-desenvolvedor-back-end-ruby-on-rails-pleno" }
   end
 end
