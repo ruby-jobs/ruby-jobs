267eead49b667fcbfe7baeea1d89e9435a3b4842
Marcelo Piva <m.pivaa@gmail.com>
2014-12-20 15:24:04 -0200

    Skills não tem suporte a markdown - teste retirado

--- spec/views/jobs/show.html.erb_spec.rb
+++ spec/views/jobs/show.html.erb_spec.rb
@@ -8,12 +8,4 @@ describe 'jobs/show.html.erb' do
       expect(rendered).to have_selector('.job-description > h1')
     end
   end
-
-  context 'skills' do
-    it 'displays the markdow as html' do
-      assign :job, create(:job, skills: '#Skills')
-      render
-      expect(rendered).to have_selector('.skills > h1')
-    end
-  end
 end
