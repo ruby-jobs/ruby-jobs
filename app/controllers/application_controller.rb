77049bedb74c4bc97df7490a460e4cf41f61f021
Luiz Cezer <lccezinha@gmail.com>
2015-05-10 19:36:33 -0300

    Setup for responders

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
+require "application_responder"
+
 class ApplicationController < ActionController::Base
+  self.responder = ApplicationResponder
+  respond_to :html, :json
+  responders :flash
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
