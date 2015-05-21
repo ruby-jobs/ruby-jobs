769ae48532a8d1cc919d9dcef3baf43c4928ce2d
Thiago Augusto <thiago.android88@gmail.com>
2015-05-21 08:47:52 -0300

    Update 20141118010059_updates_nil_jobs_location.rb

--- db/migrate/20141118010059_updates_nil_jobs_location.rb
+++ db/migrate/20141118010059_updates_nil_jobs_location.rb
@@ -3,7 +3,7 @@ class UpdatesNilJobsLocation < ActiveRecord::Migration
   	jobs = Job.where(:remote => nil)
 
   	jobs.each do |job|
-  		job.update_column(:remote, false)
+  	  job.update_column(:remote, false)
   	end
   end
 end
