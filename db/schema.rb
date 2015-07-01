07ad263cbc082b1eccb008424bc59b077e17cb5c
Rodrigo Monteiro Ferreira <rodrigomonteirof@gmail.com>
2015-07-01 16:12:39 -0300

    enable_extension plpgsql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150224002400) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.string   "location"
