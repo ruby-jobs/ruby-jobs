b9dd90c9479e077296c6d5cfd434b7ed3c980d99
Marcelo Barreto <marcelobarretojunior@gmail.com>
2016-02-15 00:08:14 -0200

    Custom devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { :omniauth_callbacks => "users/omniauth_callbacks",
+      sessions: "users/sessions", registrations: "users/registrations" }
   root 'home#index'
 
   resources :jobs, except: [:edit, :update, :destroy]
