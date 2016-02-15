e6e4a7ca9bf27bd67741cde2d5442457525162d7
Marcelo Barreto <marcelobarretojunior@gmail.com>
2016-02-15 00:06:04 -0200

    Validations and Relationship with Job

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, 
+         :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+         
+  has_many :jobs, dependent: :destroy
+  
+  validates :name, presence: true
+
+   def self.from_omniauth(auth)
+     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+       user.email = auth.info.email
+       user.password = Devise.friendly_token[0,20]
+       user.name = auth.info.name
+     end
+   end
 end
