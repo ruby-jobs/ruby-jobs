84c260d9aa63f50ba9f40addea806970e8a9f3b9
Marcelo Barreto <marcelobarretojunior@gmail.com>
2016-02-14 22:58:16 -0200

    Add omniauth to users

---
+++ db/migrate/20160215005727_add_omniauth_to_users.rb
@@ -0,0 +1,7 @@
+class AddOmniauthToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+    add_column :users, :name, :string
+  end
+end
