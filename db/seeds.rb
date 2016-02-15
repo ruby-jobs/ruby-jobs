fc77c58698c533074bf9444dd5d60a68905eedd3
Marcelo Barreto <marcelobarretojunior@gmail.com>
2016-02-15 00:08:39 -0200

    Add user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
+User.create(name: 'Ruby Jobs', email: 'ruby@jobs.me', password: 'rubyjobs')
+
 10.times do |index|
-  Job.create! title: "Job ##{index}", location: "Location ##{index}",
+  User.first.create! title: "Job ##{index}", location: "Location ##{index}",
               company: "Company ##{index}", email: "email#{index}@email.com",
               description: "Description ##{index}", modality: rand(4)
 end
