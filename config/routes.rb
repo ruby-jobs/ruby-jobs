e31ab3a5ad9e83c0ac4004a55bfaa1e17a34cb89
Marcelo Barreto <marcelobarretojunior@gmail.com>
2016-02-17 01:38:08 -0200

    Add custom routes for users#passwords

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
-                                    sessions: 'users/sessions', registrations: 'users/registrations' }
+                                    sessions: 'users/sessions', registrations: 'users/registrations',
+                                    passwords: 'users/passwords' }
 
   resources :jobs, except: [:edit, :update, :destroy]
 
