122e6774cff51469887699ac68c89cd4411fc91e
Marcelo Barreto <marcelobarretojunior@gmail.com>
2016-03-03 02:18:45 -0300

    Facebook variable on secrets.yml

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.omniauth :facebook, '225095164504125', '3631df10f43207422b13fac154f08df6'
+  config.omniauth :facebook, Rails.application.secrets.facebook_id, Rails.application.secrets.facebook_secret
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
