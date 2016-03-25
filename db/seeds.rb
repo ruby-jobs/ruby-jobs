53db1275b4de0829041b8c235c3ec48a934ce6d2
Marcelo Barreto <marcelobarretojunior@gmail.com>
2016-03-24 23:04:19 -0300

    Front end problems and associate user on a variable on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-User.create(name: 'Ruby Jobs', email: 'ruby@jobs.me', password: 'rubyjobs')
+user = User.create(name: 'Ruby Jobs', email: 'ruby@jobs.me', password: 'rubyjobs')
 
 10.times do |index|
   Job.create! title: "Job ##{index}", location: "Location ##{index}",
               company: "Company ##{index}", email: "email#{index}@email.com",
-              description: "Description ##{index}", modality: rand(4), user_id: 1
+              description: "Description ##{index}", modality: rand(4), user_id: user.id
 end
